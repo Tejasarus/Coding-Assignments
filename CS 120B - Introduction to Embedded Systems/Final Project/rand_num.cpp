2044462c2a81c23322db15adb089579bb989927b
Tejas Shah <50884660+Tejasarus@users.noreply.github.com>
2024-01-09 11:56:16 -0800

    Add files via upload

---
+++ CS 120B - Introduction to Embedded Systems/Final Project/rand_num.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int random_num()
+{
+    return 0;
+}
+
+int main()
+{
+    int rand = random_num();
+
+    cout << rand << endl;
+    
+    return 0;
+}
